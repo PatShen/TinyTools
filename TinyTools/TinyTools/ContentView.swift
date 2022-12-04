d0f81d7b576a26a873d2ea23449f1de5dca37d3f
PatShen <swxpat@qq.com>
2022-12-04 15:27:14 +0800

    Add files

---
+++ TinyTools/TinyTools/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TinyTools
+//
+//  Created by 沈闻欣 on 2022/9/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TinyTools/TinyTools/TinyToolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinyToolsApp.swift
+//  TinyTools
+//
+//  Created by 沈闻欣 on 2022/9/8.
+//
+
+import SwiftUI
+
+@main
+struct TinyToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
